4727d534b8791fb4fbd47624417359be22b77a76
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-10-16 17:07:54 +0100

    fix, link not refresh pages and others
    -Active class only when button is selected.
    -When click a button, this used route properly and not refresh all page.
    -Clean Pattern  = Create app.route.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
+import {routes} from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ImageService} from './services/image.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar.component';
-import { GalleryComponent } from './gallery/gallery.component';
 import { ImageListComponent } from './gallery/image-list/image-list.component';
 import { ImageComponent } from './gallery/image-list/image.component';
 import { ImageDetailComponent } from './gallery/image-detail/image-detail.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const appRoutes: Routes = [
-	{ path: '', redirectTo:'/gallery', pathMatch: 'full'},
-	{ path: 'gallery', component: GalleryComponent},
-	{ path: 'contact', component: ContactComponent},
-	{ path: 'about', component: AboutComponent}
-];
-
 
 @NgModule({
   declarations: [
@@ -33,7 +24,7 @@ const appRoutes: Routes = [
     ContactComponent
   ],
   imports: [
-  	RouterModule.forRoot(appRoutes),
+  	routes,
     BrowserModule
   ],
   providers: [ImageService],
